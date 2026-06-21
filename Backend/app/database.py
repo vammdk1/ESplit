16adac03e74efae3603f6618e525b32d35a6baf3
vammdk1 <victor.martinez.dk@gmail.com>
2026-06-21 12:37:21 +0200

    Creación de backend

---
+++ Backend/app/database.py
@@ -0,0 +1,14 @@
+import os
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
