74051d252deae24a307aa4db1b8b138c4cc911cf
vammdk1 <victor.martinez.dk@gmail.com>
2026-07-08 00:13:49 +0200

    Seguridad tokens en web socket

--- Backend/app/routers/ws.py
+++ Backend/app/routers/ws.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from fastapi import APIRouter, Query, WebSocket, WebSocketDisconnect, Depends
+from jose import JWTError
+import jwt
 from sqlmodel import select
+from app.auth import SECRET_KEY, ALGORITHM
 from app.connection_manager import manager
 from app.database import get_session
 from app.models import Participant
@@ -8,7 +11,32 @@ router = APIRouter()
 
 
 @router.websocket("/ws/payments/{payment_id}")
-async def payment_socket(websocket: WebSocket, payment_id: int):
+async def payment_socket(
+    websocket: WebSocket, 
+    payment_id: int,
+    token: str = Query(...)  # Token JWT
+):
+    # verificar token para aceptar la conexion
+    try :
+        pyload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = pyload["user_id"]
+    except JWTError:
+        await websocket.close(code=1008)  # Código de cierre para "Policy Violation"
+        return
+    
+    # Verificar que el usuario es miembro de la sala de pago
+    with next(get_session()) as session:
+        participant = session.exec(
+            select(Participant).where(
+                Participant.payment_id == payment_id,
+                Participant.user_id == user_id
+            )
+        ).first()
+        if not participant:
+            await websocket.close(code=1008)  # Código de cierre para "Policy Violation"
+            return
+    
+    # Conexión original del websocket, ahora que el usuario ha sido verificado
     await manager.connect(payment_id, websocket)
     try:
         while True:
